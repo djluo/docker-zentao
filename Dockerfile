3f39ea404a33744d6813f39ba16c87721e5fdfdd
djluo <dj.luo@baoyugame.com>
2015-01-31 17:20:28 +0800

    init

---
+++ Dockerfile
@@ -0,0 +1,23 @@
+FROM       docker.xlands-inc.com/baoyu/php-fpm
+MAINTAINER djluo <dj.luo@baoyugame.com>
+
+ENV SOURCE http://sourceforge.net/projects/zentao/files/6.4/ZenTaoPMS.6.4.stable.zip/download
+
+RUN export http_proxy="http://172.17.42.1:8080/" \
+    && export DEBIAN_FRONTEND=noninteractive     \
+    && apt-get update \
+    && apt-get install -y wget unzip \
+    && wget -O /zentao.zip $SOURCE   \
+    && unzip /zentao.zip \
+    && apt-get clean \
+    && unset http_proxy DEBIAN_FRONTEND \
+    && rm -rf usr/share/locale \
+    && rm -rf usr/share/man    \
+    && rm -rf usr/share/doc    \
+    && rm -rf usr/share/info
+
+ADD ./php-fpm.conf  /etc/php5/fpm/php-fpm.conf
+ADD ./entrypoint.pl /entrypoint.pl
+
+ENTRYPOINT ["/entrypoint.pl"]
+CMD        ["/usr/sbin/php5-fpm", "--fpm-config", "/etc/php5/fpm/php-fpm.conf"]
---
+++ run.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+# vim:set et ts=2 sw=2:
+
+# Author : djluo
+# version: 4.0(20150107)
+
+# chkconfig: 3 90 19
+# description:
+# processname: zentao container
+
+[ -r "/etc/baoyu/functions"  ] && source "/etc/baoyu/functions" && _current_dir
+[ -f "${current_dir}/docker" ] && source "${current_dir}/docker"
+
+# ex: ...../dir1/dir2/run.sh
+# container_name is "dir1-dir2"
+_container_name ${current_dir}
+
+images="${registry}/baoyu/zentao"
+#default_port="172.17.42.1:9292:9292"
+
+action="$1"    # start or stop ...
+_get_uid "$2"  # uid=xxxx ,default is "1000"
+shift $flag_shift
+unset  flag_shift
+
+# 转换需映射的端口号
+app_port="$@"  # hostPort
+app_port=${app_port:=${default_port}}
+_port
+
+_run() {
+  local mode="-d"
+  local name="$container_name"
+  #local cmd="ruby script/rails server webrick -e production -p9292"
+  local cmd=""
+
+  [ "x$1" == "xdebug" ] && _run_debug
+
+  sudo docker run $mode $port \
+    --link zentao-mysql:mysql \
+    -e "TZ=Asia/Shanghai"     \
+    -e "User_Id=${User_Id}"   \
+    -w "/zentaopms/"          \
+    -v ${current_dir}/logs/:/logs/ \
+    -v ${current_dir}/www/:/zentaopms/www/   \
+    --name ${name} ${images} \
+    $cmd
+}
+###############
+_call_action $action
